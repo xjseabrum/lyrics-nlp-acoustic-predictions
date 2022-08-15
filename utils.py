cc16cfc98ed64e604538c5b9253dd99830900cc6
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2022-08-14 20:27:19 -0600

    a lackidaisical update -- day 11

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # Utility functions for the project
 
-from typing import Tuple
+from typing import Iterable, Tuple
 import pandas as pd
 import re
 import math as m
@@ -8,6 +8,8 @@ import numpy as np
 from os.path import exists
 from scipy.stats import skew, kurtosis
 from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import Word2Vec
+from nltk.corpus import stopwords
 
 # If Genius didn't find lyrics/rejected the search, it returns a None type
 # Setting the length of those lyrics to be 0. Otherwise, count the number
@@ -19,28 +21,65 @@ def count_words(lyrics:str) -> int:
         return 0
     return len(re.split("\n| ", lyrics))
 
+def proportion_unique_words(lyrics:str) -> float:
+    # Proportion of unique words in the lyrics
+    if type(lyrics) != str:
+        return 0
+    if len(lyrics) == 0:
+        return 0
+    return len(set(lyrics.split())) / count_words(lyrics)
+
+def preprocess_text_for_vectorization(lyrics, tokenize = True):
+    # Pipeline for preprocessing the text
+    # strip punctation, stop word removal, tokenization
+    # and vectorization
+    if type(lyrics) != list:
+        lyrics = list(lyrics)
+    lyrics = strip_punctuation(lyrics)
+    lyrics = [x.lower() for x in lyrics]
+    lyrics = remove_stop_words(lyrics)
+    if tokenize:
+        lyrics = [x.split() for x in lyrics]
+    return lyrics
+
+
+def remove_stop_words(data_column):
+    # Remove stop words from the data_column
+    stop_words = set(stopwords.words("english"))
+    # Since these are lyrics, it's probably important to keep some 
+    # stop words in the lyrics. 
+    keep_stops = {"he", "her", "hers", "herself", "his", "i", "me", "my", "myself", "no", "not", "our", "ours", "ourselves", "she", "she's", "them", "they", "we", "you", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"}
+    stop_words -= keep_stops
+    # Split the lyrics on spaces
+    lyrics_removed_stop_words = []
+    for lyric in data_column:
+        lyric_split = lyric.split()
+        lyric_split_no_stop = [x for x in lyric_split if x not in stop_words]
+        lyrics_removed_stop_words.append(" ".join(lyric_split_no_stop))
+    return lyrics_removed_stop_words
+
 def strip_punctuation(lyrics:list):
     # Adjust for non-standard punctuation
     # Left double quote to regular double quote:
     lyrics = [x.replace("&#8220;", "\"") for x in lyrics]
-    lyrics = [x.replace("“", "\"") for x in lyrics]
+    # lyrics = [x.replace("“", "\"") for x in lyrics]
     # Right double quote to regular double quote:
     lyrics = [x.replace("&#8221;", "\"") for x in lyrics]
-    lyrics = [x.replace("”", "\"") for x in lyrics]
+    # lyrics = [x.replace("”", "\"") for x in lyrics]
     # Left single quote to regular single quote:
     lyrics = [x.replace("&#8216;", "\'") for x in lyrics]
-    lyrics = [x.replace("‘", "\'") for x in lyrics]
+    # lyrics = [x.replace("‘", "\'") for x in lyrics]
     # Right single quote to regular single quote:
     lyrics = [x.replace("&#8217;", "\'") for x in lyrics]
-    lyrics = [x.replace("â€¦", "\'") for x in lyrics]
-    lyrics = [x.replace("â€™", "\'") for x in lyrics]
-    lyrics = [x.replace("’", "\'") for x in lyrics]
+    # lyrics = [x.replace("â€¦", "\'") for x in lyrics]
+    # lyrics = [x.replace("â€™", "\'") for x in lyrics]
+    # lyrics = [x.replace("’", "\'") for x in lyrics]
     # Replace other unicode characters with regular space
     lyrics = [x.replace("\u2005", " ") for x in lyrics]
     lyrics = [x.replace("\uffef", " ") for x in lyrics]
     lyrics = [x.replace("\u205f", " ") for x in lyrics]
     lyrics = [x.replace("\n", " ") for x in lyrics]
-    punc_strip = "\?!\"#%&(),./:;<=>@[\\]^_`{|}~"
+    punc_strip = "?!\"#%&(),./:;<=>@[\\]^_`{|}~"
     for lyric in range(len(lyrics)):
         specific_lyrics = lyrics[lyric]
         specific_lyrics = specific_lyrics.translate(str.maketrans("", "", punc_strip))
@@ -139,3 +178,21 @@ def calculate_tfidf(lyrics:list) -> Tuple[pd.DataFrame, list, pd.DataFrame]:
         csr_format_representation.append(tfidf[lyric].data)
 
     return sparse_matrix, csr_format_representation, idf
+
+def create_w2v(lyrics:Iterable, vector_size = 200, min_count = 5) -> Word2Vec:
+    # Create the word2vec model
+    # the gensim library will take care of the heavy lifting
+    # and return a model object.
+
+    # Using a custom defined preprocess_text_for_vectorization function
+    lyrics = preprocess_text_for_vectorization(lyrics, tokenize = True)
+    w2v_model = Word2Vec(lyrics, min_count = min_count, 
+                         vector_size = vector_size)
+    print(f"W2V created {vector_size}-dimensional vectors " 
+          f"with {len(w2v_model.wv)} word(s) in the vocab.")
+    return w2v_model
+
+# def lyric_vectorizer():
+#     vectorizer = TextVectorization(max_tokens = 4096,
+#                                    output_sequence_length = 256)
+#     pass
