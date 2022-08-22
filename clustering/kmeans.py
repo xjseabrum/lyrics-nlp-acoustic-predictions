d89ff811be17b83b8c4577c71ad542ebc1e4fdc3
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2022-08-22 15:13:33 -0600

    Is that R I see??

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -1,1 +1,23 @@
 # K-Means Clustering
+from sklearn.cluster import KMeans
+
+# For bringing in the pickled embeddings from the Google Colab notebook
+import pickle
+import numpy as np
+import pandas as pd
+
+# For visualizing the clusters
+import matplotlib.pyplot as plt
+from yellowbrick.text import TSNEVisualizer
+
+# Load the embeddings from the Google Colab notebook
+with open('Google_Colab_Notebooks/x_train_st_emb.pkl', 'rb') as f:
+    x_train_st_emb = pickle.load(f)
+
+# Save this as a csv for use in R in a bit.
+pd.DataFrame(x_train_st_emb).to_csv("Google_Colab_Notebooks/x_train_st_emb.csv")
+
+# Determine the best number of clusters with R's NbClust
+# switch over to the optimal_k.R file for this
+# or if you already have Python/R interoperability you can use the following:
+# optimal_k = R.readRDS('optimal_k.RDS')
---
+++ clustering/optimal_k.R
@@ -0,0 +1,17 @@
+# You can't stay in Python forever!
+# R has good stat packages that don't exist or just aren't
+# quite the same as in Python.
+
+require(NbClust)
+library(NbClust)
+
+# Load in the data which were made originally in Python.
+data <- read.csv("Google_Colab_Notebooks/x_train_st_emb.csv", header = TRUE)
+
+# If you don't like the <- syntax in R you can still just use an =.
+# data = read.csv("data.csv")
+# There are some fringe cases where you DON'T want to use =
+# in place of <- .  This is due to the order of operator assignments in R. 
+# In short, <- and -> have a higher precedence than =.
+
+
