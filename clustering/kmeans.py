ef11cc01025a6707e4c496a8efd97908338fc219
xjseabrum <77007903+xjseabrum@users.noreply.github.com>
2022-08-22 18:05:23 -0600

    That's it for today

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from yellowbrick.text import TSNEVisualizer
+from distinctipy import get_colors, get_hex
 
 # Load the embeddings from the Google Colab notebook
 with open('Google_Colab_Notebooks/x_train_st_emb.pkl', 'rb') as f:
@@ -31,27 +32,65 @@ x_train.to_csv('data/05_x_train_st_emb.csv')
 # or the ``rpy2`` library in Python, you can more easily 
 # interoperate between Python and R.
 
-# Optimal number of clusters according to NbClust is 3.
-# Can use an elbow/knee graph too. 
+# Optimal number of clusters according to R's NbClust is 3.
 
-inertias = []
-for k in range(2, 10):
+# Can use an elbow/knee graph too to determine this. 
+inertiae = []
+for k in range(1, 11):
     kmeans = KMeans(n_clusters=k, random_state=0).fit(x_train_st_emb)
-    print(k, kmeans.inertia_)
-    inertias.append(kmeans.inertia_)
+    inertiae.append(kmeans.inertia_)
 
 # Plot
-i = np.arange(len(inertias))
-for idx in range(i):
-    knee = KneeLocator(i, self.distortions, 
-                        S = 1, curve='convex', 
-                        direction='decreasing', 
-                        interp_method='interp1d')
-    fig2 = plt.figure(figsize=(5, 5))
-    self.knee.plot_knee()
-    plt.xlabel("k")
-    plt.ylabel("Distortion")
+i = np.arange(1, 11)
+knee = KneeLocator(i, inertiae, 
+                    S = 1, curve = "convex", 
+                    direction = "decreasing", 
+                    interp_method = "interp1d")
+fig2 = plt.figure(figsize=(5, 5))
+knee.plot_knee()
+plt.xlabel(f"k, knee point is {knee.knee}")
+plt.ylabel("inertia")
+plt.show()
+
+# Function to use to define the clusters and visualize them
+def make_clusters_and_plot(k, data, return_data = False):  
+    # Use distinctipy's get_colors and get_hex
+    # to get a list of distinct colors automatically
+    colors = get_colors(k, pastel_factor = 0.2)
+    color_hex = [get_hex(x) for x in colors]
+    
+    X = data.filter(regex='st_emb_.*')
+    kmeans = KMeans(n_clusters = k, random_state = 0).fit(X)
+    groupings = kmeans.labels_
+    
+    
+    # TSNE will project the 768-dimensions down to 2 for visualization
+    # The projection might look messy as who knows that hyperplanes/spheres are
+    # actually separating the clusters.
+    tsne = TSNEVisualizer(random_state = 0, colors = color_hex, 
+                          alpha = 0.8)
+    tsne.fit(X, [f"Cluster: {k}" for k in groupings])
+    tsne.show()
+
+    if return_data:
+        data["groupings"] = groupings
+        return data
 
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(x_train_st_emb)
-groupings = kmeans.labels_
+# Distinct colors from:
+# https://mokole.com/palette.html
+# Hex values generated above with [Black OK, 90%] 
+# as the luminosity settings with 10000 maximum loops
+
+# Three clusters. Knee and NbClust suggest that so let's use it.
+data_3_clusters = make_clusters_and_plot(k = 3, data = x_train,  
+                                         return_data = True)
+# Five clusters
+data_5_clusters = make_clusters_and_plot(k = 5, data = x_train, 
+                                         return_data = True)
+# Seven Clusters
+data_7_clusters = make_clusters_and_plot(k = 7, data = x_train, 
+                                         return_data = True)
+# Two Clusters
+data_2_clusters = make_clusters_and_plot(k = 2, data = x_train, 
+                                         return_data = True)
